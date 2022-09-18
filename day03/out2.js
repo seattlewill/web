861aca9e350601e45878b329bcc8032a6aee676a
seattlewill <113329279+seattlewill@users.noreply.github.com>
2022-09-18 19:20:38 +0900

    Delete day03 directory

--- day03/out2.js
+++
@@ -1,6 +0,0 @@
-function add(){
-    alert('더하기 기능 처리후 결과 창')
-}
-function minus(){
-    alert('빼기 기능 처리후 결과 창')
-}
--- day03/out3.js
+++
@@ -1,6 +0,0 @@
-function go() {
-    alert('go함수를 누르셨군요.!')
-}
-function move() {
-    alert('move함수를 누르셨군요.!')
-}
